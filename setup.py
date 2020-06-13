591b8fc6942bcc062952b23d7ada07ff4645718f
Valeriu92 <valeriu.arnautu@sustainalytics.com>
2020-06-13 19:22:04 +0300

    setup docker and django project

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 setup(
     name="polls",
     version="0.1",
-    packages=find_packages('polls'),
-    package_dir={'':'polls'}
+    packages=find_packages('src/polls'),
+    package_dir={'':'src/polls'}
 )
